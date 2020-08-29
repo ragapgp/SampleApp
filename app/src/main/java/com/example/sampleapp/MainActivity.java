5a924bd0a311cea4e8c452922ec4bd9ae867e23b
ragapgp <ragapgp@gmail.com>
2020-08-29 13:09:12 -0400

    Added toast statements.

--- app/src/main/java/com/example/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sampleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Toast with Hello World message
+        Toast.makeText(this, "Hello World!!", Toast.LENGTH_SHORT).show();
+        //Toast with Hello! This is Android App.
+        Toast.makeText(this, "Hello! This is Android App!!", Toast.LENGTH_SHORT).show();
     }
 }
